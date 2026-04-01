fef51516baad0a808ad45cb692ec3e6f500c920f
Daniele <28843028+Chiaf1@users.noreply.github.com>
2026-04-01 17:02:27 +0200

    added influx query to test
    added the influx generic query and runned some tests

--- cmd/solar-api/main.go
+++ cmd/solar-api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/chiaf1/solar-api/internal/config"
+	"github.com/chiaf1/solar-api/internal/influx"
 )
 
-const CONFIG_PATH = "./config.yaml"
+const CONFIG_PATH = `C:\Generale\Progetti\Lettura produzione e consumo arduino\solar-api\config.yaml`
 
 func main() {
 	// Load configs from file
@@ -20,4 +23,19 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Println("Config Loaded")
+
+	// Creating an influx db client and testing some querys
+	infClient := influx.New(conf.InfluxDB.Url, conf.InfluxDB.Token, conf.InfluxDB.Org, conf.InfluxDB.Bucket)
+
+	//test query
+	flux := fmt.Sprintf(`from(bucket: "%s")
+	|> range(start: today(), stop: now())
+	|> filter(fn: (r) => r._measurement == "energy")
+	|> aggregateWindow(every: 1m, fn: mean)
+	|> yield()`, conf.InfluxDB.Bucket)
+	test, err := infClient.Query(context.Background(), flux)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(test)
 }
--- internal/influx/influx.go
+++ internal/influx/influx.go
@@ -1,1 +1,44 @@
 package influx
+
+import (
+	"context"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go"
+)
+
+type Client struct {
+	c      influxdb2.Client
+	org    string
+	bucket string
+}
+
+// Returns the pointer to a Client struct with the influxdb2 client
+func New(url, token, org, bucket string) *Client {
+	return &Client{
+		c:      influxdb2.NewClient(url, token),
+		org:    org,
+		bucket: bucket,
+	}
+}
+
+// Apply the flux query and returns an interface
+func (i *Client) Query(ctx context.Context, flux string) ([]map[string]interface{}, error) {
+	queryAPI := i.c.QueryAPI(i.org)
+	result, err := queryAPI.Query(ctx, flux)
+	if err != nil {
+		return nil, err
+	}
+
+	rows := make([]map[string]interface{}, 0)
+	for result.Next() {
+		rec := result.Record()
+		row := rec.Values()
+
+		rows = append(rows, row)
+	}
+	if result.Err() != nil {
+		return nil, result.Err()
+	}
+
+	return rows, nil
+}
